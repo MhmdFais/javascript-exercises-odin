d1ec019f1890e278ea93b8ea764fd155b50d4831
Muhammed <117585798+MhmdFais@users.noreply.github.com>
2023-12-30 00:16:13 +0530

    first seven ecxercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeNumbers) {
+    let newArray = [];
+    for ( let i=0; i<array.length; i++ ){
+        if ( !removeNumbers.includes(array[i]) ){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(numberOne, numberTwo) {
+    if( !Number.isInteger(numberOne) || !Number.isInteger(numberTwo) ){
+        return "ERROR";
+    }
+    if( numberOne <0 || numberTwo <0 ){
+        return "ERROR";
+    }
+    if ( numberOne > numberTwo ){
+        let temp = numberOne;
+        numberOne = numberTwo;
+        numberTwo = temp;
+    }
+    let value = 0;
+    let addBy = numberOne;
+    for ( let i=numberOne; i <= numberTwo; i++ ){
+        value = value + addBy;
+        addBy++;
+    }
+    return value; 
 };
 
 // Do not edit below this line
