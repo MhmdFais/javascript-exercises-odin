9fede5e6a2e864cf594ba90d945809460605c936
Muhammed <117585798+MhmdFais@users.noreply.github.com>
2024-01-04 23:48:03 +0530

    done 8th, 9th, and 10th quetion

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((tot, item)=> tot + item ,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((multiply, item) => multiply*item, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(arr) {
+	let fact = 1;
+  if ( arr === 0 ){
+    return 1;
+  }
+  else{
+    for ( let i=1; i<=arr; i++ ){
+      fact *= i;
+    }
+  }
+  return fact;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    let numberWeLook;
+    if ( typeof number !== 'number' ){
+        numberWeLook = parseInt(number);
+    }
+    else{
+        numberWeLook = number;
+    }
+
+    if(numberWeLook === 0){
+        return 0;
+    }
+
+    if ( numberWeLook < 0 ){
+        return 'OOPS';
+    }
+
+    let first = 1;
+    let second = 0;
+
+    for (let i = 2; i <= numberWeLook; i++) {
+        let current = first + second;
+        second = first;
+        first = current;
+    }
+
+    return first;
 };
 
 // Do not edit below this line
